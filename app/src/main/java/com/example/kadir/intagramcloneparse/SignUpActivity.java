b02b907cd205c877c679d1e29533f5e065518c5e
murakadir12 <34250985+murakadir12@users.noreply.github.com>
2019-02-23 17:41:19 +0300

    giriş ve cıkıs islemleri

--- app/src/main/java/com/example/kadir/intagramcloneparse/SignUpActivity.java
+++ app/src/main/java/com/example/kadir/intagramcloneparse/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.kadir.intagramcloneparse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
@@ -20,8 +22,27 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_up);
         usernameText = findViewById(R.id.sign_up_activity_name_text);
         passwordText = findViewById(R.id.sign_up_activity_password_text);
+        ParseUser parseUser = ParseUser.getCurrentUser();
+        if(parseUser !=null){
+            Intent intent = new Intent(getApplicationContext(),FeedAvtivity.class);
+            startActivity(intent);
+        }
     }
     public void signin(View view){
+        ParseUser.logInInBackground(usernameText.getText().toString(), passwordText.getText().toString(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+            if(e!=null){
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(getApplicationContext(),"Welcome " +user.getUsername(),Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(),FeedAvtivity.class);
+                startActivity(intent);
+            }
+            }
+        });
 
     }
     public void signup(View view){
@@ -36,7 +57,8 @@ public class SignUpActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(),e.getLocalizedMessage(),Toast.LENGTH_SHORT).show();
                 }else{
                     Toast.makeText(getApplicationContext(),"Kayıt Tamam",Toast.LENGTH_SHORT).show();
-
+                    Intent intent = new Intent(getApplicationContext(),FeedAvtivity.class);
+                    startActivity(intent);
                 }
             }
         });
